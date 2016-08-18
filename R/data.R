8a5ccaaed8ee6a07dda0387a221d06e0b18e6618
rsh249 <rsh249@cornell.edu>
2016-08-18 14:24:32 -0400

    UPDATE: Package assembles without error

---
+++ R/data.R
@@ -0,0 +1,24 @@
+#' A sample of species data
+#' 
+#' A sample of GBIF point locality occurrence data for species co-occurring at a vegetation plot in SW Virginia
+#' 
+#' @docType data
+#' @keywords datasests
+#' @name distr
+#' @usage data(distr)
+#' @format A data.frame with 5 columns
+#' 'distr'
+NULL
+
+#' Raster grid for 19 Bioclimatic Variables from the CLIMOND project
+#' 
+#' A dataset of climate model output for 19 bioclimatic variables. Original data (AND MORE) can be found here (Account required but free): https://www.climond.org
+#' 
+#' @docType data
+#' @keywords datasests
+#' @name climondbioclim
+#' @usage data(climondbioclim)
+#' @format A raster stack
+#' @source \url{https://www.climond.org}
+#climondbioclim
+NULL
