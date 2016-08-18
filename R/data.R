fbb7cff3c73d6cb993700b7aff28eee70d69defa
rsh249 <rsh249@cornell.edu>
2016-08-18 15:34:22 -0400

    UPDATE: Example climate data cropped and included in extdata

--- R/data.R
+++ R/data.R
@@ -10,6 +10,19 @@
 #' 'distr'
 NULL
 
+#' A sample of genera data
+#' 
+#' A sample of GBIF point locality occurrence data for the genera co-occurring at a vegetation plot in SW Virginia
+#' 
+#' @docType data
+#' @keywords datasests
+#' @name distrgen
+#' @usage data(distrgen)
+#' @format A data.frame with 5 columns
+#' 'distrgen'
+NULL
+
+
 #' Raster grid for 19 Bioclimatic Variables from the CLIMOND project
 #' 
 #' A dataset of climate model output for 19 bioclimatic variables. Original data (AND MORE) can be found here (Account required but free): https://www.climond.org
