219e51c972a5656b2a0d19c9f5f6045de6551c4f
rsh249 <rsh249@cornell.edu>
2017-03-21 16:48:14 -0400

    Added get data functions

--- R/rmysql.R
+++ R/rmysql.R
@@ -13,7 +13,7 @@
   }
   
   con = DBI::dbConnect(RMySQL::MySQL(), dbname='rh_div_amnh', username='rharbert', host = 'localhost');
-  query = paste("SELECT div_id, genus, species, lat, lon from div_base where genus = \'", genus, "\' and species = \'", species, "\' LIMIT =", limit, sep='');
+  query = paste("SELECT div_id, genus, species, lat, lon from div_base where genus = \'", genus, "\' and species = \'", species, "\' LIMIT ", limit, sep='');
   get = DBI::dbGetQuery(con, query);
   return(get)
 }
