e131bd6ca297c4c97e72ca203cd5d82cb0aae28f
rsh249 <rsh249@cornell.edu>
2017-03-21 16:52:38 -0400

    Added get  data functions

--- R/rmysql.R
+++ R/rmysql.R
@@ -1,7 +1,7 @@
 
 
 
-.gbif_local <- function(taxon, limit=10000000000000){
+.gbif_local <- function(taxon, limit=1000000000){
   #this will only work on Cuvier
   if(!is.null(grep(" ", taxon))){
     split <- strsplit( taxon, ' ');
@@ -11,9 +11,10 @@
     genus = taxon;
     species = '%%';
   }
-  
+  query = paste("SELECT div_id, genus, species, lat, lon from div_base where genus = \'", genus, "\' and species = \'", species, "\' LIMIT ", sprintf("%i", limit), sep='');
+  return(query)
   con = DBI::dbConnect(RMySQL::MySQL(), dbname='rh_div_amnh', username='rharbert', host = 'localhost');
-  query = paste("SELECT div_id, genus, species, lat, lon from div_base where genus = \'", genus, "\' and species = \'", species, "\' LIMIT ", limit, sep='');
   get = DBI::dbGetQuery(con, query);
   return(get)
 }
+
