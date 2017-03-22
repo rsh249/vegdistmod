f379b5f1566d37f38eac72b9ee580c29d11a4e54
rsh249 <rsh249@cornell.edu>
2017-03-22 11:51:44 -0400

    Add get data functions

--- R/rmysql.R
+++ R/rmysql.R
@@ -3,18 +3,19 @@
 
 .gbif_local <- function(taxon, limit=1000000000, db, u, h, pass){
   #this will only work on Cuvier
-  if(!is.null(grep(" ", taxon))){
+  #if(!is.null(grep("[[:space:]]", taxon))){
     split <- strsplit( taxon, ' ');
     genus = split[[1]][1];
     species = split[[1]][2];
-  } else {
-    genus = taxon;
-    species = '%%';
-  }
+    if(is.na(species)){
+      species = "%%";
+    }
+  #} 
   query = paste("SELECT div_id, genus, species, lat, lon from div_base where genus = \'", genus, "\' and species = \'", species, "\' LIMIT ", sprintf("%i", limit), sep='');
-  #return(query)
+  return(query)
   con = DBI::dbConnect(RMySQL::MySQL(), dbname=db, username=u, host = h, password = pass);
   get = DBI::dbGetQuery(con, query);
+  DBI::dbDisconnect(con);
   return(get)
 }
 
